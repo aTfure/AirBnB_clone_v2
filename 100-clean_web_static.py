fb83deffa7cc284713db6b617c275b0a163ba926
aTfure <OCHIENG.FREDONYANGO@GMAIL.COM>
2022-12-06 04:19:26 +0300

    [Feature]: a script that deletes out-of-date archives

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,15 +8,15 @@ env.user = "ubuntu"
 
 
 def do_clean(number=0):
-        """ CLEANS """
+    """ CLEANS """
 
-        number = int(number)
+    number = int(number)
 
-        if number == 0:
-            number = 2
-        else:
-            number += 1
+    if number == 0:
+        number = 2
+    else:
+        number += 1
 
-        local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
-        path = '/data/web_static/releases'
-        run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))

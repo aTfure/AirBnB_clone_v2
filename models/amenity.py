33c9bb73e0ff6aaaf2e125f74da61fe69e52e588
aTfure <OCHIENG.FREDONYANGO@GMAIL.COM>
2022-12-06 04:30:41 +0300

    [Fix]:code

--- models/amenity.py
+++ models/amenity.py
@@ -9,13 +9,13 @@ from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-        """Representation of Amenity """
-            if models.storage_t == 'db':
-                        __tablename__ = 'amenities'
-                                name = Column(String(128), nullable=False)
-                                    else:
-                                                name = ""
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
 
-                                                    def __init__(self, *args, **kwargs):
-                                                                """initializes Amenity"""
-                                                                        super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)

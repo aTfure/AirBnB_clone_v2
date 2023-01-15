c703a8f02879c2c6c241e9772de547687bd8df82
aTfure <atfure@gmail.com>
2023-01-15 23:52:20 +0000

    0x02. AirBnB clone - MySQL

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
+"""Returns Cities when route is accessed
+"""
 from flask import Flask, render_template
 from models import storage
-from models.state import State
-from models.city import City
-
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_session(exception):
-    """ Teardown """
+def teardown(exceptions):
+    """Perfoms Clean up
+    """
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def display_html():
-    """ Function called with /states_list route """
-    states = storage.all(State)
-    return render_template('8-cities_by_states.html',
-                           Table="States",
-                           states=states)
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Returns Cities when route is accessed
+    """
+    state_obj = storage.all("State")
+    city_obj = storage.all("City")
+    return render_template("8-cities_by_states.html",
+                           states=[value for value in state_obj.values()],
+                           cities=[value for value in city_obj.values()])
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
